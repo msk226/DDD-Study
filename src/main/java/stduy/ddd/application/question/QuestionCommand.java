5d4559b901e9ee1f4ba8c16eb350978aa3a57e29
msk226 <kminseok5167@gmail.com>
2025-04-01 13:20:47 +0900

    feat[UC-01]: 질문 수정 유즈 케이스 인터페이스 정의 및 구현체 작성

--- src/main/java/stduy/ddd/application/question/QuestionCommand.java
+++ src/main/java/stduy/ddd/application/question/QuestionCommand.java
@@ -3,4 +3,6 @@ package stduy.ddd.application.question;
 public class QuestionCommand {
 
     public record create(String title, String content) {}
+
+    public record update(Long questionId, String title, String content) {}
 }

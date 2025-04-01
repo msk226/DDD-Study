17160c645086f77deaed209d46c00e5d69fd2d70
msk226 <kminseok5167@gmail.com>
2025-04-01 21:07:22 +0900

    feat[UC-05]: 질문 단건 조회 유즈 케이스 엔드포인트 생성

--- src/main/java/stduy/ddd/presentation/question/QuestionController.java
+++ src/main/java/stduy/ddd/presentation/question/QuestionController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,5 +65,11 @@ public class QuestionController {
                                                                @RequestParam String keyword) {
         Page<QuestionSummary> questions = questionFindUseCase.getQuestions(pageable, keyword);
         return ResponseEntity.status(HttpStatus.OK).body(questions) ;
+    }
+
+    @GetMapping("{questionId}")
+    public ResponseEntity<QuestionSummary> findQuestion(@PathVariable Long questionId) {
+        QuestionSummary questionSummary = questionFindUseCase.getQuestion(questionId);
+        return ResponseEntity.status(HttpStatus.OK).body(questionSummary);
     }
 }

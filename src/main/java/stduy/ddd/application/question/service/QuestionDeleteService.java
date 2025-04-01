357e9ece67d4d74ecee926e9a565d3a4bd3cbffb
msk226 <kminseok5167@gmail.com>
2025-04-01 14:07:26 +0900

    feat[UC-03]: 질문 삭제 유즈 케이스 커맨드 객체 입력 받도록 수정

--- src/main/java/stduy/ddd/application/question/service/QuestionDeleteService.java
+++ src/main/java/stduy/ddd/application/question/service/QuestionDeleteService.java
@@ -3,6 +3,7 @@ package stduy.ddd.application.question.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import stduy.ddd.application.question.QuestionCommand;
 import stduy.ddd.application.question.usecase.QuestionDeleteUseCase;
 import stduy.ddd.domain.question.Question;
 import stduy.ddd.domain.question.QuestionRepository;
@@ -15,10 +16,10 @@ public class QuestionDeleteService implements QuestionDeleteUseCase {
     private final QuestionRepository questionRepository;
 
     @Override
-    public void deleteQuestion(Long questionId, Long userId) {
-        Question question = questionRepository.findById(questionId)
+    public void deleteQuestion(QuestionCommand.Delete command) {
+        Question question = questionRepository.findById(command.questionId())
                 .orElseThrow(() -> new IllegalArgumentException("질문이 존재하지 않습니다."));
 
-        question.delete(userId);
+        question.delete(command.userId());
     }
 }

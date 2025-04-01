e7983bdb422234fd7b628aa2d9bd0c28ac2c39d8
msk226 <kminseok5167@gmail.com>
2025-04-01 22:17:28 +0900

    feat[UC-08]: 답변 업데이트 엔드포인트 작성

--- src/main/java/stduy/ddd/presentation/answer/AnswerController.java
+++ src/main/java/stduy/ddd/presentation/answer/AnswerController.java
@@ -4,12 +4,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import stduy.ddd.application.answer.AnswerCommand;
 import stduy.ddd.application.answer.usecase.AnswerCreateUseCase;
+import stduy.ddd.application.answer.usecase.AnswerUpdateUseCase;
 import stduy.ddd.domain.user.UserPrincipal;
 
 @RestController
@@ -18,6 +21,7 @@ import stduy.ddd.domain.user.UserPrincipal;
 public class AnswerController {
 
     private final AnswerCreateUseCase answerCreateUseCase;
+    private final AnswerUpdateUseCase answerUpdateUseCase;
 
     @PostMapping
     public ResponseEntity<?> createAnswer(@RequestBody AnswerRequest.Create request,
@@ -25,5 +29,15 @@ public class AnswerController {
         AnswerCommand.Create command = new AnswerCommand.Create(request.content(), principal.getId(), request.questionId());
         Long answerId = answerCreateUseCase.createAnswer(command);
         return ResponseEntity.status(HttpStatus.CREATED).body(answerId);
+    }
+
+
+    @PatchMapping("{answerId}")
+    public ResponseEntity<?> updateAnswer(@RequestBody AnswerRequest.Update request,
+                                          @PathVariable Long answerId,
+                                          @AuthenticationPrincipal UserPrincipal principal) {
+        AnswerCommand.Update command = new AnswerCommand.Update(request.content(), principal.getId(), answerId);
+        Long updatedAnswerId = answerUpdateUseCase.updateAnswer(command);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedAnswerId);
     }
 }
--- src/main/java/stduy/ddd/presentation/answer/AnswerRequest.java
+++ src/main/java/stduy/ddd/presentation/answer/AnswerRequest.java
@@ -3,4 +3,6 @@ package stduy.ddd.presentation.answer;
 public class AnswerRequest {
 
     public record Create(String content, Long questionId) {}
+
+    public record Update(String content) {}
 }

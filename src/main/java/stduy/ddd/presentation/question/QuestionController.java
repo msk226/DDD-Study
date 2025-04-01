c9a229530e9949e0a07346699b8318694ba2aaf8
msk226 <kminseok5167@gmail.com>
2025-04-01 20:07:28 +0900

    feat[UC-04]: Question 페이징 조회 유즈 케이스 엔드포인트 작성

--- src/main/java/stduy/ddd/presentation/question/QuestionController.java
+++ src/main/java/stduy/ddd/presentation/question/QuestionController.java
@@ -3,21 +3,26 @@ package stduy.ddd.presentation.question;
 import static stduy.ddd.presentation.question.QuestionResponse.*;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import stduy.ddd.application.question.QuestionCommand;
 import stduy.ddd.application.question.QuestionCommand.Delete;
 import stduy.ddd.application.question.usecase.QuestionCreateUseCase;
 import stduy.ddd.application.question.usecase.QuestionDeleteUseCase;
+import stduy.ddd.application.question.usecase.QuestionFindUseCase;
 import stduy.ddd.application.question.usecase.QuestionUpdateUseCase;
-import stduy.ddd.domain.user.User;
 import stduy.ddd.domain.user.UserPrincipal;
 
 @RestController
@@ -28,6 +33,7 @@ public class QuestionController {
     private final QuestionCreateUseCase questionCreateUseCase;
     private final QuestionUpdateUseCase questionUpdateUseCase;
     private final QuestionDeleteUseCase questionDeleteUseCase;
+    private final QuestionFindUseCase questionFindUseCase;
 
     @PostMapping("")
     public ResponseEntity<Create> createQuestion(@RequestBody QuestionRequest.Create request,
@@ -51,5 +57,12 @@ public class QuestionController {
         QuestionCommand.Delete command = new Delete(request.questionId(), principal.getId());
         questionDeleteUseCase.deleteQuestion(command);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<QuestionSummary>> findQuestions(@PageableDefault Pageable pageable,
+                                                               @RequestParam String keyword) {
+        Page<QuestionSummary> questions = questionFindUseCase.getQuestions(pageable, keyword);
+        return ResponseEntity.status(HttpStatus.OK).body(questions) ;
     }
 }

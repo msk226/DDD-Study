8b4608726bef05c87b7467b9b24ef85e15a88952
msk226 <kminseok5167@gmail.com>
2025-04-01 12:47:06 +0900

    feat[UC-01]: 질문 생성 유즈 케이스 구현체 생성

---
+++ src/main/java/stduy/ddd/application/question/QuestionCreateService.java
@@ -0,0 +1,36 @@
+package stduy.ddd.application.question;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import stduy.ddd.application.question.QuestionCommand.create;
+import stduy.ddd.application.question.usecase.QuestionCreateUseCase;
+import stduy.ddd.domain.question.Question;
+import stduy.ddd.domain.question.QuestionRepository;
+import stduy.ddd.domain.question.vo.Content;
+import stduy.ddd.domain.question.vo.Title;
+import stduy.ddd.domain.user.User;
+import stduy.ddd.domain.user.UserRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class QuestionCreateService implements QuestionCreateUseCase {
+
+    private final QuestionRepository questionRepository;
+    private final UserRepository userRepository;
+
+    @Override
+    public Long createQuestion(create command, Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 회원입니다."));
+
+        Question question = Question.create(
+                new Title(command.title()),
+                new Content(command.content()),
+                user
+        );
+
+        return questionRepository.save(question).getId();
+    }
+}

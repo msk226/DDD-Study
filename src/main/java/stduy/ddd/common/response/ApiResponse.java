4d8c71969f3d973589580c4ecfd8e138284ab596
msk226 <kminseok5167@gmail.com>
2025-04-03 10:15:30 +0900

    feat: API 응답에서 Path 삭제

--- src/main/java/stduy/ddd/common/response/ApiResponse.java
+++ src/main/java/stduy/ddd/common/response/ApiResponse.java
@@ -7,18 +7,17 @@ public record ApiResponse<T>(
         String code,
         String message,
         T data,
-        LocalDateTime timestamp,
-        String path
+        LocalDateTime timestamp
 ) {
-    public static <T> ApiResponse<T> success(T data, String path) {
-        return new ApiResponse<>(200, "SUCCESS", "요청이 성공적으로 처리되었습니다.", data, LocalDateTime.now(), path);
+    public static <T> ApiResponse<T> success(T data) {
+        return new ApiResponse<>(200, "SUCCESS", "요청이 성공적으로 처리되었습니다.", data, LocalDateTime.now());
     }
 
-    public static ApiResponse<Void> success(String path) {
-        return new ApiResponse<>(200, "SUCCESS", "요청이 성공적으로 처리되었습니다.", null, LocalDateTime.now(), path);
+    public static ApiResponse<Void> success() {
+        return new ApiResponse<>(200, "SUCCESS", "요청이 성공적으로 처리되었습니다.", null, LocalDateTime.now());
     }
 
-    public static ApiResponse<?> fail(ErrorCode code, String path) {
-        return new ApiResponse<>(code.getStatus(), code.name(), code.getMessage(), null, LocalDateTime.now(), path);
+    public static ApiResponse<?> fail(ErrorCode code) {
+        return new ApiResponse<>(code.getStatus(), code.name(), code.getMessage(), null, LocalDateTime.now());
     }
 }

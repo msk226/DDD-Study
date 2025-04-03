312a01408fc8377f048c1deaa0f0694f4fc2b750
msk226 <kminseok5167@gmail.com>
2025-04-03 11:16:16 +0900

    feat: 누락된 질문 및 답변 존재 유무 검증 시, 예외 핸들링

--- src/main/java/stduy/ddd/application/questionLike/service/QuestionLikeService.java
+++ src/main/java/stduy/ddd/application/questionLike/service/QuestionLikeService.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import stduy.ddd.application.questionLike.QuestionLikeCommand;
 import stduy.ddd.application.questionLike.usecase.QuestionLikeUseCase;
+import stduy.ddd.common.response.DomainException;
+import stduy.ddd.common.response.ErrorCode;
 import stduy.ddd.domain.question.Question;
 import stduy.ddd.domain.question.repository.QuestionRepository;
 import stduy.ddd.domain.questionLike.QuestionLike;
@@ -26,10 +28,10 @@ public class QuestionLikeService implements QuestionLikeUseCase {
     public boolean toggleLike(QuestionLikeCommand.Like command) {
 
         User user = userRepository.findById(command.userId())
-                .orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 회원입니다."));
+                .orElseThrow(() -> new DomainException(ErrorCode.USER_NOT_FOUND));
 
         Question question = questionRepository.findById(command.questionId())
-                .orElseThrow(() -> new IllegalArgumentException("존재 하지 않는 질문입니다."));
+                .orElseThrow(() -> new DomainException(ErrorCode.QUESTION_NOT_FOUND));
 
         Optional<QuestionLike> like = questionLikeRepository.findByUserAndQuestion(user, question);
 

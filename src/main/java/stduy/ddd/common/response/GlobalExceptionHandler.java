564fceb373df28c4ca7f88f550920b00483ad854
msk226 <kminseok5167@gmail.com>
2025-04-03 10:12:59 +0900

    feat: 예외 핸들러 작성

---
+++ src/main/java/stduy/ddd/common/response/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package stduy.ddd.common.response;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<ApiResponse<?>> handleDomainException(DomainException e, HttpServletRequest request) {
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(ApiResponse.fail(e.getErrorCode(), request.getRequestURI()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleUnexpected(Exception e, HttpServletRequest request) {
+        return ResponseEntity.internalServerError()
+                .body(ApiResponse.fail(ErrorCode.INTERNAL_SERVER_ERROR, request.getRequestURI()));
+    }
+}
